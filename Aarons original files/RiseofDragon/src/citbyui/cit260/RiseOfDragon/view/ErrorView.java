6b9aefc05608967491ab8680707f4c97a2dd3b8f
RobertDAustin <ops23rob@gmail.com>
2016-03-26 15:16:04 -0700

    Added files via upload

---
+++ Aarons original files/RiseofDragon/src/citbyui/cit260/RiseOfDragon/view/ErrorView.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.curiousWorkmanship.view;
+
+/**
+ *
+ * @author aaronrose
+ */
+public class ErrorView {
+    
+    private static final PrintWriter errorFile = CuriousWorkmanship.getOutFile();
+    private static final PrintWriter logFile = CuriousWorkmanship.getLogFile();
+    
+    public static void display(String className, String errorMessage) {
+        
+        errorFile.println(
+                    "-------------------------------------------------------"
+                  + "\n- ERROR - " + errorMessage
+                  + "\n-------------------------------------------------------");
+        
+        // log error
+        logFile.println(className + " - " + errorMessage);
+    }
+}
