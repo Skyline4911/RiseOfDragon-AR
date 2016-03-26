00facd749c2181adafc23fb17436b8b6ff493872
RobertDAustin <ops23rob@gmail.com>
2016-03-26 15:17:43 -0700

    Update MainMenuView.java

--- Aarons original files/RiseofDragon/src/citbyui/cit260/RiseOfDragon/view/MainMenuView.java
+++ Aarons original files/RiseofDragon/src/citbyui/cit260/RiseOfDragon/view/MainMenuView.java
@@ -3,24 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package citbyui.cit260.RiseOfDragon.view;
+package byui.cit260.curiousWorkmanship.view;
 
 import byui.cit260.curiousWorkmanship.model.Player;
 import static com.sun.java.accessibility.util.EventID.MENU;
 
 /**
  *
- * @author robertaustin
+ * @author aaronrose
  */
 class MainMenuView {
     
     void displayMainMenuView() {
-        System.out.println("\n*** displayMenu() function called ***");
+        this.console.println("\n*** displayMenu() function called ***");
     }
     private void displayNextView(Player player) {
         
         // display a custom welcome message
-        System.out.println("\n======================================"
+        this.console.println("\n======================================"
                           + "\n Welcome to the game " + player.getName()
                           + "\n We hope you have a lot of fun!"
                           + "\n======================================"
@@ -30,16 +30,14 @@ class MainMenuView {
         MainMenuView mainMenuView = new MainMenuView();
         
         // Display the main menu view
-        mainMenuView.displayMainMenuView();
+        mainMenuView.display();
     }
-
-    @Override
-    public boolean doAction(String value) {
-    
-        value = value.toUpperCase(); // convert to all upper case
-    public class MainMenuView extends View {
-
-        public MainMenuView()
+    private void display() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public class mainMenuView extends View {
+        
+        public mainMenuView() {
             super("\n"
                 + "\n-----------------------------------"
                 + "\n| Main Menu                       |"
@@ -49,12 +47,12 @@ class MainMenuView {
                 + "\nS - Save game"
                 + "\nE - Exit"
                 + "\n-----------------------------------");
-        
-       public void displayManu() {
+        }
+        public void displayManu() {
             char selection = ' ';
             do {
                 
-                System.out.println(MENU); // display the main menu
+                this.console.println(MENU); // display the main menu
                 
                 String input = this.getInput(); // get the user's selection
                 selection = input.charAt(0); // get first character of string
@@ -85,37 +83,104 @@ class MainMenuView {
                 case 'E': // Exit the program
                     return;
                 default:
-                    System.out.println("\n*** Invalid selection *** Try again");
+                    this.console.println("\n*** Invalid selection *** Try again");
                     break;               
             }
         }
-        private void startNewGame() {
-            System.out.println("*** startNewGame function called ***");
+        private void startNewGame() { 
+            // create a new game
+            GameControl.createNewGame(CuriousWorkmanship.getPlayer());
+            this.console.println("*** startNewGame function called ***");
+            
+            // display a game menu
+            GameMenuView gameMenu = new GameMenuView();
+            gameMenu.dispay();
         }
         private void startExistingGame() {
-            System.out.println("*** startExistingGame function called ***");
+            this.console.println("*** startExistingGame function called ***");
+        }
+        private void startSavedGame() {
+            
+            // prompt for and get the name of the file to save the game in
+            this.console.println("\n\nEnter the file path for file where the game "
+                                + "is to be saved.");
+            String filePath = this.getInput();
+            
+            try {
+                // start a saved game
+                GameControl.getSavedGame(filePath);
+                
+        } catch (Exception ex) {
+            ErrorView.display("MainMenuView", ex.getMessage());
+        }
+            // display the game menu
+            GameMenuView gameMenu = new GameMenuView();
+            gameMenu.dispay();
         }
         private void saveGame() {
-            System.out.println("*** startSaveGame function called ***");
+            this.console.println("\n\nEnter the file path for file where the game"
+                                + "is to be saved.");
+            String filePath = this.getInput();
+            
+            try {
+                // save the game to the specified file
+                GameControl.saveGame(CuriousWorkmanship.getCurrentGame(), filePath);
+            } catch (Exception ex) {
+                ErrorView.display("MainMenuView", ex.getMessage());
+            }
         }
         private void displayHelpMenu() {
-            System.out.println("*** displayHelpMenu function called ***");
+            this.console.println("*** displayHelpMenu function called ***");
+        }
+        public void displayMap() {
+            this.console.println("*** displayMap stub function called ***");
         }
 
-        private String getInput() {
+        @Override
+        public void display() {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
+
+        @Override
+        public String getInput() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public boolean doAction(String value) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        
     }
     public class GameControl {
+
+        private static void saveGame(Game currentGame, String filePath) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        private static void getSavedGame(String filePath) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
         
-        public static void createNewGame(Player player) {
-            System.out.println("\n*** createNewGame stub function called ***");
+        public void createNewGame(Player player) {
+            this.console.println("\n*** createNewGame stub function called ***");
         }
     public class GameMenuView {
         
         void displayMenu() {
-            System.out.println("\n*** displayMenu stub function called ***");
+            this.console.println("\n*** displayMenu stub function called ***");
         }
     }
+    }
+
+    private class console {
+
+        private static void println(String_displayMenu_function_called_) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public console() {
+        }
     }
 }
