fe1bf8dad7a08a86c2371def69a8f1921712befc
RobertDAustin <ops23rob@gmail.com>
2016-03-26 15:14:13 -0700

    Update View.java

--- Aarons original files/RiseofDragon/src/citbyui/cit260/RiseOfDragon/view/View.java
+++ Aarons original files/RiseofDragon/src/citbyui/cit260/RiseOfDragon/view/View.java
@@ -1,82 +1,86 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package byui.cit260.curiousWorkmanship.view;
 
-package citbyui.cit260.RiseOfDragon.view;
-
-import byui.cit260.curiousWorkmanship.model.Player;
-import static com.sun.java.accessibility.util.EventID.MENU;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 
 /**
  *
- * @author robertaustin
+ * @author aaronrose
  */
-
 public abstract class View implements ViewInterface {
-   
+    
     protected String displayMessage;
+    private Object String;
+    
+    protected final BufferedReader keyboard = CuriousWorkmanship.getInFile();
+    protected final PrintWriter console = CuriousWorkmanship.getOutFile();
     
     public View() {
     }
     
     public View(String message) {
-        this.displayMessage = message:
+        this.displayMessage = message;
     }
     
     @Override
-    public void display() {
+    public java.lang.String display() {
         
         boolean done = false;
-        do{
+        do {
             // prompt for and get players name
-            String value = this.getInput():
+            String value = this.getInput();
             if (value.toUpperCase().equals("Q")) // user wants to quit
-                    return; // exit the view
+                return; // exit the view
             
             // do the requested action and display the next view
             done = this.doAction(value);
             
         } while (!done); // exit the view when done == true
-    }
-    
+        
     @Override
-    public String getInput() {
-        Scanner keyboard = new Scanner(System.in);
+     public String getInput() {
+        
+
         boolean valid = false;
         String value = null;
         
-        // while a valid name has not been retrieved
-        while  (!valid){
-            
-        }
-    }
-    
-    @Override
-    public String getInput() {
-    
-        Scanner keyboard = new Scanner(System.in);
-        boolean valid = false;
-        String value = null;
-
-        // while a valid name has not been retrieved
+        //while a valid name has not been retrieved
         while (!valid) {
-   
-        // prompt for the player's name
-        System.out.println("/n" + this.displayMessage);
-        
-        // get the value entered from the keyboard
-        value = keyboard.nextLine();
-        value = value.trim();
-
-        if (value.length() < 1) { // blank value entered
-            System.out.println("\n*** You must entere a value *** ");
-            continue;
+            
+            // prompt for the player's name
+            this.console.println("\n" + this.displayMessage);
+            
+            // get the value entered from the keyboard
+            value = keyboard.readLine();
+            value = value.trim();
+            
+            if (value.length() < 1) { // blank value entered 
+                this.console.println("\n*** You must enter a value *** ");
+                continue;
+            }
+            
+            break;
         }
-
-        break;
+        
+        return value; // return the name
     }
-    
-    return value; // return the name
+      
+    @Override
+     public boolean doAction(String choice) {
+        Character character = null;
+        
+        MapControl.moveCharactersToStartingLocation(character, coordinates);
+    }
+{
+{
+        
+        value = value.toUpperCase(); // convert to al upper case
+    }
+}
+    }
 }
